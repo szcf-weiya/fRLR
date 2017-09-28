de70107ce3c661f1ca53cfb1c25001deff1a7adc
szcf-weiya <2215235182@qq.com>
2017-09-28 00:26:52 -0600

    update

--- src/frlr.cpp
+++ src/frlr.cpp
@@ -1,20 +1,20 @@
 /*
  *     file fRLR/src/frlr.cpp  Fit Repeated Linear Regressions
  *     Copyright (C) 2017 Lijun Wang
- *     
+ *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
- *     
+ *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
- *     
+ *
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *    
+ *
   */
 
 #include<Rcpp.h>
@@ -37,8 +37,9 @@
 #include<gsl/gsl_cdf.h>
 #include<gsl/gsl_permutation.h>
 #include<gsl/gsl_blas.h>
+#ifdef _OPENMP
 #include<omp.h>
-
+#endif
 using namespace std;
 
 int get_col_from_r_matrix(Rcpp::NumericVector m, size_t nrow, size_t ncol, size_t i, gsl_vector* v)
